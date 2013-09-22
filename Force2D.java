42fb3b4e077bfcc4cbaefb4fa818fe71b0061450
Masoud Valafar <masoudvalafar@gmail.com>
2013-09-21 17:53:24 -0700

    moving files to correct packages

--- Force2D.java
+++
@@ -1,9 +0,0 @@
-
-
-public interface Force2D {
-	
-	public double getForceX(Coordinate2D c);
-	public double getForceY(Coordinate2D c);
-	public Coordinate2D getForce(Coordinate2D c);
-	public void draw();
-}
